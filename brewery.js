3e095798c69142ffd1368a302b777c168bf0f546
jelissa2007 <jelissa2007@gmail.com>
2021-03-18 21:40:10 -0400

    jelissa update

--- brewery.js
+++ brewery.js
@@ -1,30 +1,35 @@
 const WEATHER_KEY = config.WEATHER_API_KEY
 const UN_KEY = config.UNSPLASH_KEY
 
-const useData = (data) => {
-    console.log(data)
+
+
+const searchInput = document.getElementsByClassName("searchInput");
+const searchButn = document.getElementsByClassName("search-butn");
+const breweryList = document.getElementsByClassName("brewery-list");
+
+
+document.getElementById("searchButn").addEventListener("click", function (event) {
+    event.preventDefault();
+    const brewerySearch = event.target
+        console.log(brewerySearch.searchInput.value)
+        const breweryInfo = brewerySearch.searchInput.value 
+        
+        fetch(`https://api.openbrewerydb.org/breweries?by_state=${searchInput.value}`)
+        .then(response => response.json())
+        .then(response => breweryList(response))
+    
+
+});
+console.log("IT WORKS!!!")
+
+function addBreweryList(event){
+    event.preventDefault();
+    const breweryDiv = document.createElement("div");
+        console.log(form.breweryDiv)
+    const breweryTitle = form.breweryDiv.value 
+
+    breweryDiv.appendChild("beerList");
+
+
 }
 
-const beerURL = `https://api.openbrewerydb.org/breweries?by_state=georgia`
-
-fetch(beerURL)
-    .then(res => res.json())
-    .then(data => useData(data))
-
-let input = '30312'
-
-fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${input}&appid=${WEATHER_KEY}&units=imperial`)
-    .then(res => res.json())
-    .then(data => useData(data))
-
-
-let query = 'beer'
-
-fetch(`https://api.unsplash.com/search/photos?page=1&query=${query}`, {
-    headers: {
-    'Accept-Version': 'v1',
-    'Authorization': `Client-ID ${UN_KEY}`
-    }
-})
-    .then(res => res.json())
-    .then(data => useData(data))
